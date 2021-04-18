ccc25dd4a032c022de4742f50d8eac92343f1cc6
Dheeraj <dheerajprajapati001@gmal.com>
2021-04-18 16:13:12 +0530

    Done

--- lib/utils/my_colors.dart
+++ lib/utils/my_colors.dart
@@ -2,6 +2,12 @@ import 'package:flutter/material.dart';
 
 class MyColors{
 
-  static Color blackClr = Color(0xff000000);
+  static Color blackClr = Colors.black;
+  static Color whiteClr = Colors.white;
+  static Color lightGreenClr = Color(0xff83C124);
+  static Color darkGreenClr = Color(0xff31BE4D);
+  static Color greyClr = Color(0xff9A9191);
+  static Color greyBorderClr = Color(0xff707070);
+  static Color iconClr = Color(0xff2D5767);
 
 }
